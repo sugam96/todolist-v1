14ab80c0b7f2fda09f5918b6f6bca184551c20b0
Sugam <sharma_sugam@live.com>
2023-03-12 15:28:17 -0400

    add EJS templates

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const express = require("express");
 const bodypasrer = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-    res.send("Hello World");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 6 || currentDay === 0)
+        day = "Weekend";
+    else
+        day = "Weekday";
+    res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
